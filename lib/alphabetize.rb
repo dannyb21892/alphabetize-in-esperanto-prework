3921c1e6460d69dc45e4e014248b6e6001e8754c
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-18 05:04:21 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,13 +8,11 @@ def alphabetize(arr)
   while sorted == 0
     current = 0
     switched = 0
-    
     while current < arr.length-1
       a = nospace[current]
       b = nospace[current + 1]
       i = 0
       switch = 0
-      
       while i < [a.length, b.length].min
         if alphabet.index(a[i]) > alphabet.index(b[i])
           switch = 1
@@ -24,16 +22,13 @@ def alphabetize(arr)
         end
         i += 1
       end
-      
       if switch == 1
         arr[current], arr[current + 1] = arr[current + 1], arr[current]
         nospace[current], nospace[current + 1] = nospace[current + 1], nospace[current]
         switched = 1
       end
-      
       current += 1
     end
-    
     if switched == 0
       sorted = 1
     end
